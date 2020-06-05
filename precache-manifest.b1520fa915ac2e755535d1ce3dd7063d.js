b515e1a8b51f1c4570790cf0b90c492f2f8f20ef
Jonathan Lazzeri <jonnylazzeri94@gmail.com>
2020-06-05 17:05:01 +0300

    Updates

---
+++ precache-manifest.b1520fa915ac2e755535d1ce3dd7063d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa7471d35a28512fb830fb20ae15ee1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "102acaa67afda874e22e",
+    "url": "/monsters-rolodex/static/css/main.196bf364.chunk.css"
+  },
+  {
+    "revision": "1d6d326f4470ed67fe85",
+    "url": "/monsters-rolodex/static/js/2.46e2921d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.46e2921d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "102acaa67afda874e22e",
+    "url": "/monsters-rolodex/static/js/main.dc586680.chunk.js"
+  },
+  {
+    "revision": "aa1aeb4ed7e62e71a38e",
+    "url": "/monsters-rolodex/static/js/runtime-main.2292ee2f.js"
+  }
+]);
